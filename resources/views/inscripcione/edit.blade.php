6b1a7669c190503f01f299a700e6736e1f37be9e
mati <matiexe31@gmail.com>
2022-05-06 16:15:15 -0300

    se actualiza vista edit

--- resources/views/inscripcione/edit.blade.php
+++ resources/views/inscripcione/edit.blade.php
@@ -1,31 +1,34 @@
 @extends('layouts.app')
 
 @section('template_title')
-    Update Inscripcione
+Update Inscripcione
 @endsection
 
 @section('content')
-    <section class="content container-fluid">
-        <div class="">
-            <div class="col-md-12">
+<section class="section">
+    <div class="section-header">
+        <h3 class="page__heading">editar inscripcion</h3>
+    </div>`
+    <div class="">
+        <div class="col-md-12">
 
-                @includeif('partials.errors')
+            @includeif('partials.errors')
 
-                <div class="card card-default">
-                    <div class="card-header">
-                        <span class="card-title">Update Inscripcione</span>
-                    </div>
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('inscripciones.update', $inscripcione->id) }}"  role="form" enctype="multipart/form-data">
-                            {{ method_field('PATCH') }}
-                            @csrf
+            <div class="card card-default">
+                <!-- <div class="card-header">
+                    <span class="card-title">Update Inscripcione</span>
+                </div> -->
+                <div class="card-body">
+                    <form method="POST" action="{{ route('inscripciones.update', $inscripcione->id) }}" role="form" enctype="multipart/form-data">
+                        {{ method_field('PATCH') }}
+                        @csrf
 
-                            @include('inscripcione.form')
+                        @include('inscripcione.form')
 
-                        </form>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 @endsection
--- resources/views/inscripcione/form.blade.php
+++ resources/views/inscripcione/form.blade.php
@@ -3,12 +3,12 @@
 
         <div class="form-group">
             {{ Form::label('alumno') }}
-            {{ Form::select('alumno_id',$alumno, $inscripcione->nombre, ['class' => 'form-control' . ($errors->has('alumno') ? ' is-invalid' : ''), 'placeholder' => ' ']) }}
+            {{ Form::select('alumno_id',$alumno, $inscripcione->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => ' ']) }}
             {!! $errors->first('alumno_id', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         <div class="form-group">
             {{ Form::label('grupo') }}
-            {{ Form::select('grupo_id',$curso, $inscripcione->nombre_grupo, ['class' => 'form-control' . ($errors->has('grupo_id') ? ' is-invalid' : ''), 'placeholder' => ' ']) }}
+            {{ Form::select('grupo_id',$curso, $inscripcione->nombre_grupo, ['class' => 'form-control' . ($errors->has('nombre_grupo') ? ' is-invalid' : ''), 'placeholder' => ' ']) }}
             {!! $errors->first('grupo_id', '<div class="invalid-feedback">:message</div>') !!}
         </div>
 
